32ecd5e4009d79f50e807bbe91dd9d32f6c6cb45
pierrewebdev <patrick.pierre000@gmail.com>
2021-09-25 13:55:34 -0400

    managed to get the carousel component buttons to light up when they are on a new slide; only problem is this will definitely break if I have more than 2 slides at a time in the carousel

--- app.js
+++ app.js
@@ -3,51 +3,66 @@ class Carousel{
         this.carousel = parentContainer;
     }
 
-    moveBetweenSlides(){
-        let parent = this.carousel;
-        setTimeout(function(){
-            let activeSlide = parent.querySelector('.slide.translate-x-0');
-            activeSlide.classList.remove('translate-x-0');
-            activeSlide.classList.add('-translate-x-full');
+    // moveBetweenSlides(){
+    //     let parent = this.carousel;
+    //     setTimeout(function(){
+    //         let activeSlide = parent.querySelector(".slide.translate-x-0.active");
+    //         activeSlide.classList.remove("translate-x-0", "active");
+    //         activeSlide.classList.add("-translate-x-full");
             
-            let nextSlide = activeSlide.nextElementSibling;
-            nextSlide.classList.remove('translate-x-full');
-            nextSlide.classList.add('translate-x-0');
-        }, 3000);
-    }
+    //         let nextSlide = activeSlide.nextElementSibling;
+    //         nextSlide.classList.remove("translate-x-full");
+    //         nextSlide.classList.add("translate-x-0", "active");
+    //     }, 3000);
+    // }
 
     nextSlide(){
         let parent = this.carousel;
-        let activeSlide = parent.querySelector('.slide.translate-x-0');
+        let activeSlide = parent.querySelector(".slide.translate-x-0.active");
         let nextSlide = activeSlide.nextElementSibling;
 
         if(nextSlide !== null && nextSlide.classList.contains("slide")){
-            activeSlide.classList.remove('translate-x-0');
-            activeSlide.classList.add('-translate-x-full');
+            activeSlide.classList.remove("translate-x-0", "active");
+            activeSlide.classList.add("-translate-x-full");
             
-            nextSlide.classList.remove('translate-x-full');
-            nextSlide.classList.add('translate-x-0');
+            nextSlide.classList.remove("translate-x-full");
+            nextSlide.classList.add("translate-x-0", "active");
         }
+
+        //switch button color to represent you are now on a new slide
+        this.switchActiveBtn();
     }
 
     previousSlide(){
         let parent = this.carousel;
-        let activeSlide = parent.querySelector('.slide.translate-x-0');
+        let activeSlide = parent.querySelector(".slide.translate-x-0.active");
         let previousSlide = activeSlide.previousElementSibling;
 
         if(previousSlide !== null && previousSlide.classList.contains("slide")){
-            activeSlide.classList.remove('translate-x-0');
-            activeSlide.classList.add('translate-x-full');
+            activeSlide.classList.remove("translate-x-0", "active");
+            activeSlide.classList.add("translate-x-full");
             
-            previousSlide.classList.remove('-translate-x-full');
-            previousSlide.classList.add('translate-x-0');
+            previousSlide.classList.remove("-translate-x-full");
+            previousSlide.classList.add("translate-x-0", "active");
         }
 
-        //need to write a check to make sure there is a slide before this one
+        //switch button color to represent you are now on a new slide
+        this.switchActiveBtn();
     }
 
     setActiveBtnColor(){
-        //code goes here
+        let parent = this.carousel;
+        let activeBtn = parent.querySelector("button.active");
+        activeBtn.classList.add("bg-yellow-600", "border-yellow-600");
+    }
+
+    switchActiveBtn(){
+        let parent = this.carousel;
+        let activeBtn = parent.querySelector("button.active");
+        let inactiveBtn = parent.querySelector("button:not(.active)");
+
+        activeBtn.classList.remove("active", "bg-yellow-600", "border-yellow-600");
+        inactiveBtn.classList.add("active", "bg-yellow-600", "border-yellow-600");
     }
 
     setEventListerners(){
@@ -62,10 +77,12 @@ class Carousel{
             this.nextSlide()
         });
 
+        this.setActiveBtnColor();
+
     }
 }
 
 const carouselContainer = document.querySelector(".carousel-parent");
 const carousel = new Carousel(carouselContainer);
 carousel.setEventListerners();
-carousel.moveBetweenSlides();
+// carousel.moveBetweenSlides();
